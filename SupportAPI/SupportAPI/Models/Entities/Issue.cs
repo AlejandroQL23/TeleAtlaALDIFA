6ae8e12dcd961e85fd50213682a826df44dd61d1
AlejandroQL23 <ale23200@gmail.com>
2021-06-22 09:27:01 -0600

    Creacion del proyecto TeleAtlantico

---
+++ SupportAPI/SupportAPI/Models/Entities/Issue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace SupportAPI.Models.Entities
+{
+    public partial class Issue
+    {
+        public int Id { get; set; }
+        public string Reference { get; set; }
+        public string Classification { get; set; }
+        public string Status { get; set; }
+        public DateTime? IssueTimeStamp { get; set; }
+        public string ResolutionComment { get; set; }
+        public DateTime? CreationDate { get; set; }
+        public string CreationUser { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public string UpdateUser { get; set; }
+    }
+}
---
+++ SupportAPI/SupportAPI/Models/Entities/Notes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace SupportAPI.Models.Entities
+{
+    public partial class Notes
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime? NoteTimeStamp { get; set; }
+        public DateTime? CreationDate { get; set; }
+        public string CreationUser { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public string UpdateUser { get; set; }
+    }
+}
