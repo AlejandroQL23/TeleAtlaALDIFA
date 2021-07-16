e94bb2e85d80706c0fbc42e91182f61b4075afd5
AlejandroQL23 <ale23200@gmail.com>
2021-07-16 15:27:28 -0600

    Arreglo en UI error Injectable

--- TeleAtlaCliente/src/app/pages/examples/mainclientpage/mainclientpage.component.ts
+++ TeleAtlaCliente/src/app/pages/examples/mainclientpage/mainclientpage.component.ts
@@ -5,7 +5,7 @@ import { RestService } from "src/app/rest.service";
 
 import { Subscription } from 'rxjs';
 
-import { SortService } from './SortService';
+// import { SortService } from './SortService';
 
 @Component({
   selector: "app-mainclientpage",
@@ -19,40 +19,40 @@ export class MainclientpageComponent implements OnInit {
    issues:any = [];
    issuesId:any = [];
   constructor(private fb: FormBuilder, private route: ActivatedRoute,
-    private rest:RestService, private router: Router, private sortService: SortService) { }
+    private rest:RestService, private router: Router) { }
 
 
-    @Input('sortable-column')
-    columnName: string;
+    // @Input('sortable-column')
+    // columnName: string;
 
-    @Input('sort-direction')
-    sortDirection: string = '';
+    // @Input('sort-direction')
+    // sortDirection: string = '';
 
 
-    private columnSortedSubscription: Subscription;
+    // private columnSortedSubscription: Subscription;
 
-    @HostListener('click')
-    sort() {
-        this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
-        this.sortService.columnSorted({ sortColumn: this.columnName, sortDirection: this.sortDirection });
-    }
+    // @HostListener('click')
+    // sort() {
+    //     this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    //     this.sortService.columnSorted({ sortColumn: this.columnName, sortDirection: this.sortDirection });
+    // }
 
     
 
   ngOnInit() {
     this.getIssue();
-     // subscribe to sort changes so we can react when other columns are sorted
-     this.columnSortedSubscription = this.sortService.columnSorted$.subscribe(event => {
-      // reset this column's sort direction to hide the sort icons
-      if (this.columnName != event.sortColumn) {
-          this.sortDirection = '';
-      }
-  });
+  //    // subscribe to sort changes so we can react when other columns are sorted
+  //    this.columnSortedSubscription = this.sortService.columnSorted$.subscribe(event => {
+  //     // reset this column's sort direction to hide the sort icons
+  //     if (this.columnName != event.sortColumn) {
+  //         this.sortDirection = '';
+  //     }
+  // });
   }
 
-  ngOnDestroy() {
-    this.columnSortedSubscription.unsubscribe();
-}
+//   ngOnDestroy() {
+//     this.columnSortedSubscription.unsubscribe();
+// }
 
   getIssue() {
     this.issues = [];
